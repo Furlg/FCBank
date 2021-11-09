f7a5f63a8c12a9ebfbedc9a5ee5a5aa9617bd537
shajianbin <sha19941220@163.com>
2021-11-09 21:56:39 +0800

    基版重新提交

--- fcservice/src/main/java/com/FcserviceApplication.java
+++ fcservice/src/main/java/com/FcserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.E001")
+@MapperScan("com.trade")
 public class FcserviceApplication {
 
     public static void main(String[] args) {
