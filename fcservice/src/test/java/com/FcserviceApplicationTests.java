fa7f0675e1a8a04fdff5dcd450a96b3d099faf5a
shajianbin <sha19941220@163.com>
2021-11-10 01:04:32 +0800

    完善项目

--- fcservice/src/test/java/com/FcserviceApplicationTests.java
+++ fcservice/src/test/java/com/FcserviceApplicationTests.java
@@ -1,10 +1,19 @@
 package com;
 
+import com.trade.D001.T0001.Entity.UserEntityKey;
+import com.trade.D001.T0001.Service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.Callable;
 
 @SpringBootTest
 class FcserviceApplicationTests {
+
+    @Autowired
+    UserService userService;
+
 
     @Test
     void contextLoads() {
